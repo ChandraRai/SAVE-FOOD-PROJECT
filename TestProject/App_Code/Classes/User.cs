e685ff0071ded27a61c6fc06c87d7d606d3da7c5
[Siyanthan] <vijisiyanthan@gmail.com>
2019-09-20 15:14:01 -0400

    Add User Manager and User Class

---
+++ TestProject/App_Code/Classes/User.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+
+    public User()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+
+    }
+    public User(string _username, string _email, string _phone, string _firstName, string _lastName,string _password)
+    {
+        username = _username;
+        email = _email;
+        phone = _phone;
+        firstName = _firstName;
+        lastName = _lastName;
+        privilege = 0;
+        password = Sha1(Salt(_password));
+    }
+
+    public User (string _uID,string _username, string _email, string _phone, string _firstName, string _lastName, int _privilege)
+    {
+        uId = _uID;
+        username = _username;
+        email = _email;
+        phone = _phone;
+        firstName = _firstName;
+        lastName = _lastName;
+        privilege = _privilege;
+    }
+
+    public User(string username, string password)
+    {
+        this.username = username;
+        this.password = Sha1(Salt(password));
+    }
+
+    //Getter and Setters
+    public string uId { get; private set; }
+    public string username { get; private set; }
+    public string email { get; private set; }
+    public string phone { get; private set; }
+    public string firstName { get; private set; }
+    public string lastName { get; private set; }
+    public int privilege { get; private set; }
+    public string password { get; private set; }
+
+
+    /// <summary>
+    /// The Sha1
+    /// </summary>
+    /// <param name="text">The text<see cref="string"/></param>
+    /// <returns>The <see cref="string"/></returns>
+    public static string Sha1(string text)
+    {
+        byte[] clear = System.Text.Encoding.UTF8.GetBytes(text);
+        byte[] hash = new SHA1CryptoServiceProvider().ComputeHash(clear);
+        return BitConverter.ToString(hash).Replace("-", "").ToLower();
+    }
+
+    /// <summary>
+    /// Password Encryption
+    /// </summary>
+    /// <param name="text">The text<see cref="string"/></param>
+    /// <returns>The <see cref="string"/></returns>
+    public static string Salt(string text)
+    {
+        return
+          "zu5QnKrH4NJfOgV2WWqV5Oc1l" +
+          text +
+          "1DMuByokGSDyFPQ0DbXd9rAgW";
+    }
+}
---
+++ TestProject/App_Code/Managers/UserManager.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserManager
+/// </summary>
+public class UserManager
+{
+    private static string connStr = ConfigurationManager.ConnectionStrings["savefood"].ConnectionString;
+
+    public UserManager()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public static bool validateUser(string username,string password)
+    {
+        User user = new User(username, password);
+        
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            conn.Open();
+            string sql = "SELECT Username from USERS where Username = @username and Password = @password";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@username", user.username);
+            cmd.Parameters.AddWithValue("@password", user.password);
+            return cmd.ExecuteScalar() is string;
+        }
+    }
+
+    public static bool addUser(User user)
+    {
+        SqlConnection conn = new SqlConnection(connStr);
+        SqlCommand comm;
+        comm = new SqlCommand("INSERT INTO USERS (FirstName, LastName, Username, Password, Email, Phone)" +
+            "VALUES (@first, @last, @username, @password, @email, @phone)", conn);
+
+        comm.Parameters.AddWithValue("@first", user.firstName);
+        comm.Parameters.AddWithValue("@last", user.lastName);
+        comm.Parameters.AddWithValue("@username", user.username);
+        comm.Parameters.AddWithValue("@password", user.password);
+        comm.Parameters.AddWithValue("@email", user.email);
+        comm.Parameters.AddWithValue("@phone", user.phone);
+
+        try
+        {
+            conn.Open();
+            comm.ExecuteNonQuery();
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+    /// <summary>
+    /// Zhi Wei Su - 300899450
+    /// This method checks if the username already exists
+    /// </summary>
+    public static bool UsernameExists(string username)
+    {
+        using (SqlConnection conn = new SqlConnection(connStr))
+        {
+            conn.Open();
+            SqlCommand comm = new SqlCommand("SELECT Username From Users WHERE Username = @Username", conn);
+            comm.Parameters.AddWithValue("@Username", username);
+
+            var exists = comm.ExecuteScalar();
+
+            if (exists != null)
+            {
+                return true;
+            }
+            else
+                return false;
+
+        }
+    }
+
+    public static User getUser(string username)
+    {
+        SqlDataReader reader;
+        SqlConnection conn;
+        SqlCommand comm;
+        User user = new User();
+
+
+        string connectionString = ConfigurationManager.ConnectionStrings["savefood"].ConnectionString;
+        conn = new SqlConnection(connectionString);
+        comm = new SqlCommand("SELECT Id,Username,Email,Phone,FirstName,LastName,Privilege" +
+            " From USERS WHERE Username = @userName", conn);
+        comm.Parameters.AddWithValue("@userName",username);
+
+
+        try
+        {
+            conn.Open();
+            reader = comm.ExecuteReader();
+            while (reader.Read())
+                user = new User(reader["Id"].ToString(), reader["Username"].ToString(), reader["Email"].ToString(), reader["Phone"].ToString(), 
+                    reader["FirstName"].ToString(), reader["LastName"].ToString(),Int32.Parse(reader["Privilege"].ToString()));
+
+            reader.Close();
+            return user;
+            
+        }
+        catch
+        {
+            return user;
+        }
+        finally
+        {
+            conn.Close();
+        }
+
+    }
+}
