23517c7280a26faba8e7ba261822009ef64508e4
[Siyanthan] <vijisiyanthan@gmail.com>
2019-09-21 18:38:02 -0400

    Add food and Food manager class

---
+++ TestProject/App_Code/Classes/Food.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Food
+/// </summary>
+public class Food
+{
+    public Food()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    //Getter and Setters
+    public string FId { get; private set; }
+    public string FoodName { get; private set; }
+    public string email { get; private set; }
+    public string phone { get; private set; }
+    public string firstName { get; private set; }
+    public string lastName { get; private set; }
+    public int privilege { get; private set; }
+    public string password { get; private set; }
+}
---
+++ TestProject/App_Code/Managers/FoodManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for FoodManager
+/// </summary>
+public class FoodManager
+{
+    public FoodManager()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
