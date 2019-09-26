14d2f4e5261448f90dae4484a8a0543637901bee
[Siyanthan] <vijisiyanthan@gmail.com>
2019-09-26 11:29:06 -0400

    Add UserRequest and RequestManager Classes
    -Added classes to handle user requests for food

---
+++ TestProject/App_Code/Classes/UserRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for UserRequest
+/// </summary>
+public class UserRequest
+{
+    public UserRequest()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public UserRequest(string username,string _itemType,string _itemDetails,string _ammount)
+    {
+        user = UserManager.getUser(username,"Username");
+        ItemType = _itemType;
+        ItemDetails = _itemDetails;
+        Amount = _ammount;
+        Status = 0;
+        Date = DateTime.Now;
+    }
+    //Getter and Setters
+    public string URId { get; private set; }
+    public string ItemType { get; set; }
+    public string ItemDetails { get; set; }
+    public string Amount { get; set; }
+    public DateTime Date { get; set; }
+    public User user { get; private set; }
+    public int Status { get; set; }
+}
---
+++ TestProject/App_Code/Managers/RequestManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for RequestManager
+/// </summary>
+public class RequestManager
+{
+    private static string connStr = ConfigurationManager.ConnectionStrings["savefood"].ConnectionString;
+
+    public RequestManager()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static void addRequest(UserRequest request)
+    {
+        SqlConnection conn;
+        SqlCommand comm;
+        string query = "INSERT INTO UserRequest (UId, ItemType, ItemDetails, Amount, Date, Status) " +
+            "VALUES (@UId, @ItemType, @ItemDetails, @Amount, @Date, @Status)";
+        conn = new SqlConnection(connStr);
+        comm = new SqlCommand(query, conn);
+        comm.Parameters.AddWithValue("@UId", request.user.uId);
+        comm.Parameters.AddWithValue("@ItemType", request.ItemType);
+        comm.Parameters.AddWithValue("@ItemDetails", request.ItemDetails);
+        comm.Parameters.AddWithValue("@Amount", request.Amount);
+        comm.Parameters.AddWithValue("@Date", request.Date);
+        comm.Parameters.AddWithValue("@Status", request.Status);
+
+
+        try
+        {
+            conn.Open();
+            comm.ExecuteNonQuery();
+        }
+        catch
+        {
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+}
